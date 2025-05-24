e2d80a1510e8482d73f7b1993c7b87d8599988b1
Gaurav Yadav <gyadav187@gmail.com>
2025-05-24 07:58:04 +0530

    custom react done

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./chai";
 
 function App() {
+	const username = "This is username";
 	return (
 		<>
 			<Chai />
-			<h1>This is h1</h1>
+			<h1>This is h1 {username}</h1>
 		</>
 	);
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,13 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 function MyApp() {
 	return (
 		<div>
-			<h1>Custom</h1>
+			<h1>CustomApp is star</h1>
 		</div>
 	);
 }
 
-createRoot(document.getElementById("root")).render(<MyApp />);
+const reactElement = {
+	type: "a",
+	props: {
+		href: "https://google.com",
+		target: "_blank",
+	},
+	children: "Click me to visit google",
+};
+
+const anotherElement = (
+	<a href="https://www.google.com" target="_blank">
+		Click me I am cool
+	</a>
+);
+
+const anotherReactElement = React.createElement(
+	"a",
+	{ href: "https://google.com", target: "_blank" },
+	"Do not waste your time here"
+);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
