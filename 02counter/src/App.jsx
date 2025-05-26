a43aa741ede9186842333b79ab84042062de66a7
Gaurav Yadav <gyadav187@gmail.com>
2025-05-26 13:26:19 +0530

    Tailwind

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	let [counter, setCounter] = useState(0);
+
+	function increaseCounter() {
+		if (counter <= 19) {
+			setCounter(counter + 1);
+		}
+	}
+
+	function decreaseCounter() {
+		if (counter > 0) {
+			setCounter(counter - 1);
+		}
+	}
+	return (
+		<>
+			<h1>Chai aur Gaurav</h1>
+			<h2>Counter value: {counter}</h2>
+			<button onClick={increaseCounter}>Increase Value {counter}</button>
+			<br />
+			<br />
+			<button onClick={decreaseCounter}>Decrease Value {counter}</button>
+			<p>Footer: {counter}</p>
+		</>
+	);
+}
+
+export default App;
