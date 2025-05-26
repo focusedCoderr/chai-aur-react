83d8d84bffb9979e777212ff4344ac75827791a9
Gaurav Yadav <gyadav187@gmail.com>
2025-05-26 22:07:19 +0530

    ..

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,37 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const myObj = {
+		name: "Learn me",
+		what: "JS",
+		cool: true,
+	};
 
-  return (
-    <>
-      <h1 className="bg-amber-700 text-black border p-4 rounded-2xl mb-2">
-        Tailwind Test
-      </h1>
-      <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
-        <img
-          src="https://source.unsplash.com/300x300/?random"
-          alt=""
-          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
-        />
-        <div className="mt-6 mb-2">
-          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
-            Title
-          </span>
-          <h2 className="text-xl font-semibold tracking-wide">
-            Lorem ipsum dolor
-          </h2>
-        </div>
-        <p className="text-gray-300">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
-          tempora ipsum soluta amet
-        </p>
-      </div>
-    </>
-  );
+	const notMyObj = {
+		name: "Don't Learn",
+		what: "Shortcuts",
+		cool: false,
+	};
+
+	return (
+		<>
+			<h1 className="bg-amber-700 text-black border p-4 rounded-2xl mb-2">
+				Tailwind Test
+			</h1>
+
+			<Card name="Gaurav" anObj={myObj} />
+			<Card anObj={notMyObj} />
+		</>
+	);
 }
 
-export default App
+export default App;
