faa848cbb8fb60bd3688f5fb7bab1989b4f40303
Gaurav Yadav Ubuntu <gyadav187@gmail.com>
2025-05-26 18:42:54 +0530

    .

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -8,28 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <h1 className="bg-amber-700 text-black border p-4 rounded-2xl mb-2">
+        Tailwind Test
+      </h1>
+      <div className="max-w-xs p-6 rounded-md shadow-md bg-black">
+        <img
+          src="https://source.unsplash.com/300x300/?random"
+          alt=""
+          className="object-cover object-center w-full rounded-md h-72 bg-gray-500"
+        />
+        <div className="mt-6 mb-2">
+          <span className="block text-sm font-medium font-mono tracking-widest uppercase text-indigo-400">
+            Title
+          </span>
+          <h2 className="text-xl font-semibold tracking-wide">
+            Lorem ipsum dolor
+          </h2>
+        </div>
+        <p className="text-gray-300">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+          tempora ipsum soluta amet
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
